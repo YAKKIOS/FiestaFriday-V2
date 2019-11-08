86449566c300f96531d0f6b69655453128eb29c7
Jack Johnson <jackjohnosn036@gmail.com>
2019-11-08 15:38:25 +0000

    Lots of changes here and there inc mobile friendly

--- jquery.js
+++ jquery.js
@@ -28,15 +28,15 @@ $(document).ready(function() {
     $(window).scroll( function(){
     
         /* Check the location of each desired element */
-        $('.feature-inner-block').each( function(i){
+        $('.feature-inner-block, .feature-block-alt, .centre-inner-block').each( function(i){
             
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object - 500 ){
+            if( bottom_of_window > bottom_of_object - 600 ){
                 
-                $(this).animate({'opacity':'1'},500);
+                $(this).animate({'opacity':'1'},600);
                     
             }
             
